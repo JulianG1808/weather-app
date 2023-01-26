93c101ae236c219e3b27d9c8303a8fc8fca9fdcf
Julian Gomez <juliangomez.xvii@gmail.com>
2023-01-26 13:41:11 -0300

    add function for forecast and weather

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
+import { WEATHER_API_URL, WEATHER_API_KEY} from './api';
+import { useState } from 'react';
 
 function App() {
 
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (dataSearch) => {
     const [lat, lon] = dataSearch.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async(response) => {
+        const weatherResponse = await response[0].json()
+        const forecastResponse = await response[1].json()
+
+        setCurrentWeather({ city: dataSearch.label , ...weatherResponse})
+        setForecast({ city: dataSearch.label , ...forecastResponse})
+
+        
+      })
+      .catch((err) => console.log(err))
   }
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
