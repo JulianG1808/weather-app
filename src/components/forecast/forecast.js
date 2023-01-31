e46be2164d1c45d8f4fbcb479578e54f310fb2f0
Julian Gomez <juliangomez.xvii@gmail.com>
2023-01-31 13:13:42 -0300

    add functions for wind degrees and uppercase letters

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -23,11 +23,44 @@ const Forecast = ({ data }) => {
     WEEK_DAYS.slice(0, dayInAWeek)
   );
 
+  const UpperCaseLetter = (el) => {
+    const newString = el.charAt(0).toUpperCase() + el.slice(1)
+
+    return newString
+  }
+
+  const degreesWind = (el) => {
+    if(el > 337.5 || el < 22.5) {
+      return 'N'
+    }
+    else if(el >= 22.5 && el < 67.5) {
+      return 'NE'
+    }
+    else if(el > 67.5 && el < 112.5) {
+      return 'E'
+    }
+    else if(el > 112.5 && el < 157.5) {
+      return 'SE'
+    }
+    else if(el > 157.5 && el < 202.5) {
+      return 'S'
+    }
+    else if(el > 202.5 && el < 247.5) {
+      return 'SW'
+    }
+    else if(el > 247.5 && el < 292.5) {
+      return 'W'
+    }
+    else if(el > 292.5 && el < 337.5) {
+      return 'NW'
+    }
+  }
+
   return (
     <>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, idx) => (
+        {data.list.slice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
               <AccordionItemButton>
@@ -39,7 +72,7 @@ const Forecast = ({ data }) => {
                   />
                   <label className="day">{forecastDays[idx]}</label>
                   <label className="description">
-                    {item.weather[0].description}
+                    {UpperCaseLetter(item.weather[0].description)}
                   </label>
                   <label className="min-max">
                     {Math.floor(item.main.temp_min)}°C /{" "}
@@ -63,8 +96,8 @@ const Forecast = ({ data }) => {
                   <label>{item.main.feels_like}°C</label>
                 </div>
                 <div className="daily-details-grid-item">
-                  <label>Wind speed:</label>
-                  <label>{item.wind.speed} m/s</label>
+                  <label>Wind:</label>
+                  <label>{degreesWind(item.wind.deg)} | {item.wind.speed} m/s</label>
                 </div>
                 <div className="daily-details-grid-item">
                   <label>Clouds</label>
